89f09508508f44a3399c56a9612d320fc4e7f45a
fdAssis <fd07ass@gmail.com>
2021-04-08 17:01:00 -0300

    javascript examples

--- capitulo_2_Os_metacaracteres.js
+++ capitulo_2_Os_metacaracteres.js
@@ -2,21 +2,23 @@
  * Metacaracteres tipo Representante
  */
 
+function return_reg_match(frase, reg) {
+  // link para referencia função 'match' : https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/match
+
+  return console.log(
+    ` Frase: "${frase}"\n Reg: ${reg}\n Resultado: ${frase.match(reg)}\n`
+  );
+}
+
 // Ponto
 const frase = 'O casal que estava no casamento foram para casa juntos';
 const reg_ponto = /casa./g;
-
-// link para referencia função 'match' : https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/match
-const result_reg_ponto = frase.match(reg_ponto);
-
-console.log({ result_reg_ponto });
+return_reg_match(frase, reg_ponto);
 
 // Lista
 
 const palavras =
   'não fale nao teclado Teclado estendido extendido eztendido neo n-o';
 
-const reg_lista_1 = /[ão]/g;
-const result_reg_lista_1 = palavras.match(reg_lista_1);
-
-console.log(result_reg_lista_1);
+const reg_lista_1 = /n[ãa]o/g;
+return_reg_match(palavras, reg_lista_1);
